c483aeb91d82910a6cd11b8d9d2b49cbc5d94820
Guillaume Turri <g.turri@criteo.com>
2025-05-15 15:40:20 +0200

    Fix missing } for brainfuck interpreter

--- brainfuck-interpreter/csharp/BrainfuckInterpreter/BrainfuckInterpreter.cs
+++ brainfuck-interpreter/csharp/BrainfuckInterpreter/BrainfuckInterpreter.cs
@@ -1,13 +1,14 @@
 ﻿namespace BrainfuckInterpreter
 {
-	public class BrainfuckInterpreter
-	{
-		public static IEnumerable<byte> Run(string program)
-		{
-			// TODO: write an actual implementation
-			return new byte[] { };
-		}
+    public class BrainfuckInterpreter
+    {
+        public static IEnumerable<byte> Run(string program)
+        {
+            // TODO: write an actual implementation
+            return new byte[] { };
+        }
 
         public static string RunAndGetString(string program) =>
             System.Text.Encoding.Default.GetString(Run(program).ToArray());
+    }
 }
